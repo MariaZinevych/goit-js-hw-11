9e6fc2facf2bcf222fb14a66f49e7cf9167a0c1e
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-07-09 22:27:48 +0200

    fixed fetch

--- src/index.js
+++ src/index.js
@@ -4,26 +4,34 @@ const onImage = document.querySelector('.gallery');
 const onForm = document.querySelector('.search-form');
 const onInput = document.querySelector('[type = text]');
 const axios = require('axios').default;
+const onloadMore = document.querySelector('.load-more');
+let pageMore = 1;
+
+onloadMore.addEventListener('click', searcheImage);
 onForm.addEventListener('submit', searcheImage);
 
-async function getUser() {
-  try {
-    const response = await axios.get('/user?ID=12345');
-    console.log(response);
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
 function searcheImage(e) {
+  //   onloadMore.style.display = 'none';
   e.preventDefault();
   const arrInput = onInput.value;
   fetch(
-    `https://pixabay.com/api/?key=19462317-9864fc13c2867a4042c7cafcd&q=${arrInput}&image_type=photo&orientation=horizontal&safesearch=true`
+    `https://pixabay.com/api/?key=19462317-9864fc13c2867a4042c7cafcd&q=${arrInput}&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=${pageMore}`
   )
     .then(response => response.json())
-      .then(data => { onImage.innerHTML = '';  arrImage(data.hits); }
-      )
+    .then(data => {
+      onImage.innerHTML = '';
+      arrImage(data.hits);
+    })
+    .then(() => pageMore++)
     .catch(error => {
       if (error) showError();
     });

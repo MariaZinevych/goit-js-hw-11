dd67abbe900ac01663d84e4660b3065939274759
MariaZinevych <123274407+MariaZinevych@users.noreply.github.com>
2023-07-09 16:27:15 +0200

    create gallery

---
+++ src/index.js
@@ -0,0 +1,73 @@
+import Notiflix from 'notiflix';
+
+const onImage = document.querySelector('.gallery');
+const onForm = document.querySelector('.search-form');
+const onInput = document.querySelector('[type = text]');
+const axios = require('axios').default;
+onForm.addEventListener('submit', searcheImage);
+
+async function getUser() {
+  try {
+    const response = await axios.get('/user?ID=12345');
+    console.log(response);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function searcheImage(e) {
+  e.preventDefault();
+  const arrInput = onInput.value;
+  fetch(
+    `https://pixabay.com/api/?key=19462317-9864fc13c2867a4042c7cafcd&q=${arrInput}&image_type=photo&orientation=horizontal&safesearch=true`
+  )
+    .then(response => response.json())
+      .then(data => { onImage.innerHTML = '';  arrImage(data.hits); }
+      )
+    .catch(error => {
+      if (error) showError();
+    });
+}
+
+function cardImage({
+  webformatURL,
+  largeImageURL,
+  tags,
+  likes,
+  views,
+  comments,
+  downloads,
+}) {
+  const createImage = `<div class="photo-card">
+  <img src="${largeImageURL}" alt="" loading="lazy" />
+  <div class="info">
+    <p class="info-item">
+      <b>Likes</b>${likes}
+    </p>
+    <p class="info-item">
+      <b>Views</b>${views}
+    </p>
+    <p class="info-item">
+      <b>Comments</b>${comments}
+    </p>
+    <p class="info-item">
+      <b>Downloads</b>${downloads}
+    </p>
+  </div>
+</div>;
+`;
+
+  onImage.insertAdjacentHTML('beforeend', createImage);
+}
+
+function arrImage(obj) {
+  for (const i of obj) {
+    cardImage(i);
+  }
+}
+function showError() {
+  Notiflix.Report.failure(
+    'Sorry, there are no images matching your search query.',
+    'Please try again.'
+  );
+}
